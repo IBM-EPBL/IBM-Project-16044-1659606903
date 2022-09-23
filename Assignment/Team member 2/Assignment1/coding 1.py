822a49213533cbe7ee73a82dbe67dc656022dac0
keerthananatarajan35 <113966547+keerthananatarajan35@users.noreply.github.com>
2022-09-23 16:16:47 +0530

    Add files via upload

---
+++ Assignment/Team member 2/Assignment1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[11,12,13,14,15]
+print(list)
+
+#insertion
+list.insert(5,20)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-5)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("My")
+str2=("World")
+print(str1+str2)
+
+#reverse of String
+str3="Operation"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment/Team member 2/Assignment1/coding1.output1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\HP\Desktop\Assignment\Team member 2\Assignment1\coding 1.py
+[11, 12, 13, 14, 15]
+[11, 12, 13, 14, 15, 20]
+[12, 13, 14, 15, 20]
+[12, 13, 14, 15, 20, -5]
+[-5, 12, 13, 14, 15, 20]
+[-5, 12, 13, 14, 15]
+[15, 14, 13, 12, -5]
+MyWorld
+noitarepO
+pr
