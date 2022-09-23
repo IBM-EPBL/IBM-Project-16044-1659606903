d84d4557c0557b5ed8347804f88330b2707b5d0d
SwethaSelvam16 <113839248+SwethaSelvam16@users.noreply.github.com>
2022-09-23 16:06:38 +0530

    Add files via upload

---
+++ Assignment/Team leader/Assignment1/coding1.output1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Users\HP\Downloads\coding 1 (3).py ================
+[6, 7, 8, 9, 10]
+[6, 7, 8, 9, 10, 15]
+[7, 8, 9, 10, 15]
+[7, 8, 9, 10, 15, -5]
+[-5, 7, 8, 9, 10, 15]
+[-5, 7, 8, 9, 10]
+[10, 9, 8, 7, -5]
+MyWorld
+noitarepO
+pr
