acc639dc98f27004a6130b17ac159706be338eb3
Nivetha-ac12 <113966788+Nivetha-ac12@users.noreply.github.com>
2022-09-23 16:22:55 +0530

    Add files via upload

---
+++ Assignment/Team member 3/Assignment1/coding1.output1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\HP\Desktop\Assignment\Team member 3\Assignment1\coding 1.py
+[15, 14, 13, 12, 11]
+[15, 14, 13, 12, 11, 30]
+[14, 13, 12, 11, 30]
+[14, 13, 12, 11, 30, -5]
+[-5, 11, 12, 13, 14, 30]
+[-5, 11, 12, 13, 14]
+[14, 13, 12, 11, -5]
+MyWorld
+noitarepO
+pr
